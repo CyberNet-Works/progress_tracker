f29463954b9a2224bdcb469b9ccf485ff2fa7f0f
CyberNet-Works <ronn.baharav@gmail.com>
2024-05-11 15:34:16 +0300

    booleans

---
+++ Python/python_competition/boolean/#validate triple function.py
@@ -0,0 +1,4 @@
+#validate triple function
+
+def triple_function(a, b, c):
+    return True if a == b == c == True else False
---
+++ Python/python_competition/boolean/def validate_solution(proposed_solution,.py
@@ -0,0 +1,2 @@
+def validate_solution(proposed_solution, correct_solution):
+    return "Correct" if proposed_solution == correct_solution else "Incorrect"
