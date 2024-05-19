b89fd6c817f6ef3fac4e83459d44885235289103
CyberNet-Works <ronn.baharav@gmail.com>
2024-05-19 18:29:28 +0100

    string manipulation

---
+++ Python/Python_challenges/string manipulations/convert numbers string stream to list an.py
@@ -0,0 +1,11 @@
+convert numbers string stream to list and sum
+def memorial_cost(prices):
+    prices_list = prices.split()
+    integer_price_list = []
+
+    # for element in prices_list:
+    #     element = int(element)
+    #     integer_price_list.append(element)
+    integer_price_list = [int(element) for element in prices_list]
+
+    return sum(integer_price_list)
