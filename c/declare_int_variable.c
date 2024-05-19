422407f760ce5e19bfc58e2aa30fcee6737ee25c
CyberNet-Works <ronn.baharav@gmail.com>
2024-05-19 23:31:31 +0100

    new intro to c files

---
+++ c/declare_int_variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int age = 25;
+
+    printf("%d", age);
+
+    return (0);
+
+}
---
+++ c/print_and_store_float_variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    float number = 36.43;
+
+    printf("%f", number);
+
+    return (0);
+
+}
