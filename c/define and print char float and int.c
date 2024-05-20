9fec9d33937a9c38541cd28322e48904a7f950ef
CyberNet-Works <ronn.baharav@gmail.com>
2024-05-20 10:36:18 +0100

    c file basic variables

---
+++ c/define and print char float and int.c
@@ -0,0 +1,21 @@
+// Replace ___ with your code
+
+#include <stdio.h>
+
+int main() {
+
+int number1;
+float number2;
+char letter;
+
+
+number1 = 101;
+number2 = 86.99;
+letter = 'z';
+
+    printf("%d\n", number1);
+    printf("%f\n", number2);
+    printf("%c", letter);
+
+    return 0;
+}
