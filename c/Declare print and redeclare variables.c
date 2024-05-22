b4240199ce99b2ee123fe9cd580b0f13bcb3e6c2
CyberNet-Works <ronn.baharav@gmail.com>
2024-05-22 16:16:44 +0100

    c files

---
+++ c/Declare print and redeclare variables.c
@@ -0,0 +1,14 @@
+// Replace ___ with your code
+
+#include <stdio.h>
+
+int main() {
+  
+    int number;
+    number = 31;
+    printf("%d", number);
+    number = 99;
+    printf("\n%d%", number);
+    
+    return 0;
+}
---
+++ c/variable reassignment.c
@@ -0,0 +1,20 @@
+// Replace ___ with your code
+
+#include <stdio.h>
+
+int main() {
+
+    int distance;
+    distance = 135;
+
+    printf("%d", distance);
+   
+    
+    int newDistance;
+    newDistance = 429;
+    distance = newDistance;
+
+    printf("\n%d", distance);
+
+    return 0;
+}
