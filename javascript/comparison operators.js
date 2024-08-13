b3dd83a60dcb966c91a97a045773482f0227b7a0
CyberNet-Works <ronn.baharav@gmail.com>
2024-08-13 04:08:52 +0100

    comparison operators

---
+++ javascript/comparison operators.js
@@ -0,0 +1,22 @@
+//Comparison Operators: > and >=
+
+let number1 = 21;
+let number2 = 18;
+
+// 21 greater than 21 is false
+console.log(number1 > 21); 
+ 
+// 21 greater than or equal to 21 is true
+console.log(number1 >= 21);
+ 
+// 18 greater than 21 is false
+console.log(number2 > number1);
+ 
+// 18 greater than or equal to 18 is true
+console.log(number2 >= number2);
+ 
+// 18 greater than 17 is true
+console.log(number2 > 17);
+ 
+// 18 greater than or equal to 18 is true
+console.log(number2 >= 18);
